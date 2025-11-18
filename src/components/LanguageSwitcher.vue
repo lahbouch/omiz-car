a07aa17a55a47171dd0421929e26dad76e49e40d
elviso <lahbouch.dev@gmail.com>
2025-11-18 23:52:32 +0100

    feat(LanguageSwitcher): enhance language switcher with dropdown and flag icons
    - Replaced the language selection dropdown with a dropdown menu featuring flag icons for each language
    - Updated the component to use computed properties for dynamic flag display based on the selected language
    - Added scoped styles for flag icons to improve visual presentation

--- src/components/LanguageSwitcher.vue
+++ src/components/LanguageSwitcher.vue
@@ -1,20 +1,63 @@
 <template>
-  <div class="language-select">
-    <select
-      class="form-select"
-      @change="switchLanguage($event.target.value)"
-      :value="currentLanguage"
+  <div class="nav-item dropdown has-arrow flag-nav nav-item-box">
+    <a
+      class="nav-link dropdown-toggle"
+      data-bs-toggle="dropdown"
+      href="javascript:void(0);"
+      role="button"
     >
-      <option value="en">English</option>
-      <option value="fr">Français</option>
-      <option value="es">Español</option>
-    </select>
+      <img :src="currentFlag" alt="Language" class="img-fluid flag-icon" />
+    </a>
+    <ul class="dropdown-menu p-2">
+      <li>
+        <a
+          href="javascript:void(0);"
+          class="dropdown-item"
+          @click="switchLanguage('en')"
+        >
+          <img
+            src="@/assets/admin/img/flags/gb.svg"
+            alt=""
+            class="flag-icon"
+          />English
+        </a>
+      </li>
+      <li>
+        <a
+          href="javascript:void(0);"
+          class="dropdown-item"
+          @click="switchLanguage('fr')"
+        >
+          <img
+            src="@/assets/admin/img/flags/france.svg"
+            alt=""
+            class="flag-icon"
+          />Français
+        </a>
+      </li>
+      <li>
+        <a
+          href="javascript:void(0);"
+          class="dropdown-item"
+          @click="switchLanguage('es')"
+        >
+          <img
+            src="@/assets/admin/img/flags/es.png"
+            alt=""
+            class="flag-icon"
+          />Español
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import { ref, computed } from "vue";
 import { useI18n } from "vue-i18n";
+import flagEn from "@/assets/admin/img/flags/gb.svg";
+import flagFr from "@/assets/admin/img/flags/france.svg";
+import flagEs from "@/assets/admin/img/flags/es.png";
 
 export default {
   name: "LanguageSwitcher",
@@ -22,12 +65,16 @@ export default {
     const { locale } = useI18n();
 
     const languages = {
-      en: { name: "English" },
-      fr: { name: "Français" },
-      es: { name: "Español" },
+      en: { name: "English", flag: flagEn },
+      fr: { name: "Français", flag: flagFr },
+      es: { name: "Español", flag: flagEs },
     };
 
     const currentLanguage = ref(locale.value || "en");
+
+    const currentFlag = computed(() => {
+      return languages[currentLanguage.value]?.flag || languages.en.flag;
+    });
 
     const switchLanguage = (lang) => {
       currentLanguage.value = lang;
@@ -43,8 +90,26 @@ export default {
 
     return {
       currentLanguage,
+      currentFlag,
       switchLanguage,
     };
   },
 };
 </script>
+
+<style scoped>
+.flag-icon {
+  border-radius: 50%;
+  object-fit: cover;
+  width: 20px;
+  height: 20px;
+  display: inline-block;
+  vertical-align: middle;
+}
+
+.dropdown-item img.flag-icon {
+  width: 16px;
+  height: 16px;
+  margin-right: 8px;
+}
+</style>
